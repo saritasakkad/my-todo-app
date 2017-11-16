06c02c6840becf230ba425ceee21f588b5a78f04
saritasakkad <akkadsaritas@gmail.com>
2017-11-16 16:20:26 +0100

    v2.0

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ var datepickerDefault = new MtrDatepicker(
 	  target: "demo",
 	}
 );
+
+
+// shuitcher entre 2 pages
+
+
+var rajouter = document.getElementById('bouton');
+var maincontainer = document.getElementById('maincontainer');
+var maincontainer2 = document.getElementById('maincontainer2');
+var x = true;
+rajouter.addEventListener('click',changeContenu);
+
+function changeContenu() {
+if(x == true) {
+maincontainer.style.display = 'none';
+maincontainer2.style.display = 'flex';
+rajouter.style.transform = 'rotate(45deg)';
+x = false;
+} else {
+maincontainer.style.display = 'flex';
+maincontainer2.style.display = 'none';
+rajouter.style.transform = 'rotate(0deg)';
+x = true;
+}
+}
